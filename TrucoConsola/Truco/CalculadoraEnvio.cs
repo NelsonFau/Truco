bb9e6feba39e31a1bbe9a68f169afd23ff291b42
Nelson Álvarez <nelsonalvarez_0118@outlook.es>
2025-09-02 14:53:49 -0300

    actualizando funcionalidad CalcularEnvido

--- TrucoConsola/Truco/CalculadoraEnvio.cs
+++ TrucoConsola/Truco/CalculadoraEnvio.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrucoConsola.Cartas;
 
 namespace TrucoConsola.Truco
 {
@@ -10,7 +11,25 @@ namespace TrucoConsola.Truco
     {
         public void CalcularEnvido(Rabon rabon)
         {
-           
+            Dictionary<Jugador,int> resultados = new Dictionary<Jugador, int>();
+
+            foreach (var jugador in rabon.Jugadores)
+            {
+                List<Carta> mano = jugador.Mano;
+
+
+                IEnumerable<IGrouping<string,Carta>> agruparXMano = mano.GroupBy((c) => c.Palo);
+                int maxEnvido = 0;
+
+                foreach(var grupo in agruparXMano)
+                {
+                    List<Carta> cartas = grupo.ToList();
+
+                    if(cartas.Count> 2)
+                    {
+                        
+                    }
+                }
+            }
         }
-    }
 }
